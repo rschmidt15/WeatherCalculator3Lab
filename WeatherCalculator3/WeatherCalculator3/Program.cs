3fc2aa71fb5801449efc71f0dc55509ddc9de33b
rschmidt15 <59981860+rschmidt15@users.noreply.github.com>
2020-02-04 16:49:11 -0600

    Update Program.cs

--- WeatherCalculator3/WeatherCalculator3/Program.cs
+++ WeatherCalculator3/WeatherCalculator3/Program.cs
@@ -14,51 +14,58 @@ namespace WeatherCalculator3
             int relativeHumidity;
             double heatIndex;
             string input = "";
-
-            // Get the temperature from the user
-            Console.WriteLine("Enter the temperature");
-
-            input = Console.ReadLine();
-
-            // convert "input" string variable  to an integer and store in "temperature" variables
-            temperature = int.Parse(input);
-
-            if (temperature > 50)
+            string userinput1 = "no";
+            while (userinput1 == "no")
             {
-                // Get the relative humidity from the user
-                Console.WriteLine("Enter the relative humidity");
+                // Get the temperature from the user
+                Console.WriteLine("Enter the temperature");
 
-                // NOTE: reusing the "input" variable to get the relative humidity
                 input = Console.ReadLine();
 
-                // convert "input" string variable to an integer and store in "relativeHumidity" variable
-                relativeHumidity = int.Parse(input);
+                // convert "input" string variable  to an integer and store in "temperature" variables
+                temperature = int.Parse(input);
 
-                // Calculate the heat index
-                // 0.5 * (T + 61.0 + ((T-68.0)*1.2) + (RH*0.094))
-                heatIndex = 0.5 * (temperature + 61.0 + ((temperature - 68.0) * 1.2) + (relativeHumidity * 0.094));
+                if (temperature > 50)
+                {
+                    // Get the relative humidity from the user
+                    Console.WriteLine("Enter the relative humidity");
 
-                // Write the heat index out to the console
-                Console.WriteLine("The heat index is " + heatIndex);
+                    // NOTE: reusing the "input" variable to get the relative humidity
+                    input = Console.ReadLine();
+
+                    // convert "input" string variable to an integer and store in "relativeHumidity" variable
+                    relativeHumidity = int.Parse(input);
+
+                    // Calculate the heat index
+                    // 0.5 * (T + 61.0 + ((T-68.0)*1.2) + (RH*0.094))
+                    heatIndex = 0.5 * (temperature + 61.0 + ((temperature - 68.0) * 1.2) + (relativeHumidity * 0.094));
+
+                    // Write the heat index out to the console
+                    Console.WriteLine("The heat index is " + heatIndex);
+                }
+                else
+                {
+                    // Get the wind 49speed from the user
+                    Console.WriteLine("Enter the wind speed");
+
+                    // NOTE: reusing the "input" variable to get the wind speed
+                    input = Console.ReadLine();
+
+                    // convert "input" string variable to an integer and store in "windSpeed" variable
+                    windSpeed = int.Parse(input);
+
+                    // Calculate the wind chill
+                    windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+
+                    // Write the windchill out to the console
+                    Console.WriteLine("The wind chill is " + windChill);
+                }
+                Console.WriteLine("are you done?");
+                Console.WriteLine("yes or no");
+                userinput1 =Console.ReadLine();
+
+
             }
-            else
-            {
-                // Get the wind 49speed from the user
-                Console.WriteLine("Enter the wind speed");
-
-                // NOTE: reusing the "input" variable to get the wind speed
-                input = Console.ReadLine();
-
-                // convert "input" string variable to an integer and store in "windSpeed" variable
-                windSpeed = int.Parse(input);
-
-                // Calculate the wind chill
-                windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
-
-                // Write the windchill out to the console
-                Console.WriteLine("The wind chill is " + windChill);
-            }
-            
             // Tell the user the program is ending
             Console.WriteLine("Program has ended...Thank you!");
         }
